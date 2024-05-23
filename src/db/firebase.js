7bf9ae923c8e241b2700ff539508cf7424b00a82
willson-alflen <willson.alflen@gmail.com>
2024-05-23 13:05:32 -0300

    remove .env file from remote repository

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -17,7 +17,6 @@ import { getStorage, ref, uploadString, getDownloadURL } from 'firebase/storage'
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
   authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_REACT_APP_DATABASE_URL,
   projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
   storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
